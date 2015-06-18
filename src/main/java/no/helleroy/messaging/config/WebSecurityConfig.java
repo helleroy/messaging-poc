3a8a0811ba378139adf36f141d52d39c6c85b8ff
Henrik Hellerøy <henrik.helleroy@bekk.no>
2015-06-18 13:43:34 +0200

    Authentication

---
+++ src/main/java/no/helleroy/messaging/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package no.helleroy.messaging.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll()
+                .and().csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("password").roles("USER")
+                .and().withUser("user2").password("password").roles("USER");
+    }
+}
