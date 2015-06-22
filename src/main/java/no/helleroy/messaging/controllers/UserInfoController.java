85af3cab0015a3bd81ff5d20ac3b168047706b84
Henrik Hellerøy <henrik.helleroy@bekk.no>
2015-06-22 16:08:53 +0200

    REST-endpoint for getting information on logged in user

--- src/main/java/no/helleroy/messaging/controllers/UserInfoController.java
+++ src/main/java/no/helleroy/messaging/controllers/UserInfoController.java
@@ -2,28 +2,36 @@ package no.helleroy.messaging.controllers;
 
 import no.helleroy.messaging.domain.ConnectionMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
 @RestController
+@RequestMapping("/users")
 public class UserInfoController {
 
     @Autowired
     private SessionRegistry sessionRegistry;
 
-    @RequestMapping("/users")
+    @RequestMapping
     public List<ConnectionMessage> users() {
         return sessionRegistry.getAllPrincipals()
                 .stream()
                 .map(User.class::cast)
                 .map(u -> new ConnectionMessage(u.getUsername(), true))
                 .collect(toList());
+    }
+
+    @RequestMapping("/myself")
+    public Object myself(Principal principal) {
+        return ((UsernamePasswordAuthenticationToken) principal).getPrincipal();
     }
 }
 
